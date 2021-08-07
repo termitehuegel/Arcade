4f7380d2a82b3090abdf85cf74afca755deaf108
Termitehuegel <64739941+termitehuegel@users.noreply.github.com>
2021-08-07 22:31:24 +0200

    Rework and expansion of the Game class. Game now features the automatic drawing of the Title and the Back Button. The game will now be drawn in drawGame and drawMenu => update will only be used for updating the game logic.

--- js/Game.js
+++ js/Game.js
@@ -1,14 +1,52 @@
 /**
+ * @description handles click events  => checks for ending game
+ * @param e
+ */
+function gameClickEventHandler(e) {
+    if (!game.status) {
+        let rect = canvas.getBoundingClientRect();
+        ctx.font = canvas.height/15 + 'px Arial';
+        if (clickInRect(e.clientX - rect.left, e.clientY - rect.top, 0, canvas.height*14/15, ctx.measureText('Back').width + 8, canvas.height/15)) {
+            game.end();
+        }
+    }
+}
+/**
+ * @description handles key events => checks for ending game
+ * @param e
+ */
+function gameKeyEventHandler(e) {
+    if (!game.status) {
+        if (e.key) {
+            switch (e.key) {
+                case 'Backspace':
+                    game.end();
+                    break;
+                case 'Escape':
+                    game.end();
+                    break;
+            }
+        }
+    }
+}
+
+/**
  * @property {string} name
  * @property {HTMLCanvasElement} canvas
  * @property {CanvasRenderingContext2D} context
  * @property {Arcade} arcade
+ * @property {boolean} status
  */
 class Game {
     /**
      * @type {string} the name of the Game
      */
     static name;
+    /**
+     * @description true => in the game, false => in the menu
+     * @type {boolean} the status of the game
+     */
+    status;
 
     /**
      * @constructor
@@ -20,6 +58,9 @@ class Game {
         this.canvas = canvas;
         this.context = context;
         this.arcade = arcade;
+        //init event handlers
+        document.addEventListener('keydown', gameKeyEventHandler);
+        canvas.addEventListener('click', gameClickEventHandler);
     }
 
     /**
@@ -27,12 +68,47 @@ class Game {
      */
     update() {
 
+        this.draw();
     }
 
     /**
      * @description used to end the game
      */
     end() {
+        document.removeEventListener('keydown', gameKeyEventHandler);
+        canvas.removeEventListener('click', gameClickEventHandler);
         this.arcade.end();
+    }
+
+    startGame() {}
+
+    draw() {
+        //clears the canvas
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        if (this.status) {
+            this.drawGame();
+        } else {
+            this.drawMenu();
+        }
+    }
+
+    drawGame() {}
+
+    drawMenu() {
+        //draws header
+        this.context.font = this.canvas.height/15 + 'px Arial';
+        this.context.fillStyle = '#000';
+        this.context.textAlign = 'center';
+        this.context.fillText(this.constructor.name, this.canvas.width/2, this.canvas.height/15, this.canvas.width);
+
+        //draws back to menu button
+        this.context.font = this.canvas.height/15 + 'px Arial';
+        this.context.fillStyle = '#bbb';
+        this.context.fillRect(0, this.canvas.height*14/15, this.context.measureText('Back').width + 8, this.canvas.height/15);
+        this.context.fillStyle = '#000';
+        //draws back to arcade menu button text
+        this.context.textAlign = 'left';
+        this.context.fillText('Back', 2, this.canvas.height - this.canvas.height/100, this.canvas.width)
+       ;
     }
 }
