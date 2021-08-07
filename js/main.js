47e849e12e3721e7fb57794d9d0782df1fc42082
Termitehuegel <64739941+termitehuegel@users.noreply.github.com>
2021-08-07 22:26:46 +0200

    Added canvas auto resize

--- js/main.js
+++ js/main.js
@@ -1,10 +1,24 @@
+function windowResizeEventHandler() {
+   let height = window.innerHeight;
+   let width = window.innerWidth;
+
+   if (width < height) {
+       canvas.width = width -20;
+       canvas.height = width -20;
+   } else {
+       canvas.width = height -20;
+       canvas.height = height -20;
+   }
+}
+
 let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext('2d');
-//global variable because it is used in listeners
-let blockblockListenersArcade = false;
+//window resize listener
+window.addEventListener('resize', windowResizeEventHandler)
+windowResizeEventHandler();
 //global variable because it is used in listeners
 let game;
 //name needs to be arcade because of listeners
-let arcade = new Arcade([TicTacToe, Minesweeper, Dice, Snake], canvas, ctx);
+let arcade = new Arcade([Dice, Dice, Dice], canvas, ctx);
 //draws the arcade menu for the first time
 arcade.draw();
